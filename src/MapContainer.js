58079ba683f41a3c02633a0dadcc5a797fd1ef5d
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-07-22 16:13:06 +0200

    refactor: Make MapContainer functional

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,32 +1,28 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        initialCenter={{
-          lat: 52.237049,
-          lng: 21.017532
-        }}
-      >
+const MapContainer = ({google}) => (
+  <Map
+    google={google}
+    zoom={14}
+    initialCenter={{
+      lat: 52.237049,
+      lng: 21.017532
+    }}
+  >
 
-        <Marker
-          name={'Królikarnia'}
-          position={{ lat: 52.189091, lng: 21.028411}}
-        />
+    <Marker
+      name={'Królikarnia'}
+      position={{ lat: 52.189091, lng: 21.028411}}
+    />
 
-        {/* <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div>
-        </InfoWindow> */}
-      </Map>
-    );
-  }
-}
+    {/* <InfoWindow onClose={this.onInfoWindowClose}>
+      <div>
+        <h1>{this.state.selectedPlace.name}</h1>
+      </div>
+    </InfoWindow> */}
+  </Map>
+);
 
 export default GoogleApiWrapper({
   apiKey: ('AIzaSyA9VEL4Dc3zPp_y25SUmFDZOtC73O3dzSQ')
