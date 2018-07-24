e00a48f6b603f88c08b8b69cb8f130315dd56c74
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-07-24 11:11:07 +0200

    feat: Add places json

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as places from './places.json';
+
 class App extends Component {
   render() {
     return (
